12edf899f6e053b4163f0ab8ed9412ef03c0d348
TF8088 <tiagofarinha03@outlook.com>
2023-06-21 20:39:01 +0100

    Name to Username

--- src/database/entity/userEntity.js
+++ src/database/entity/userEntity.js
@@ -5,7 +5,7 @@ const UserSchema = new EntitySchema({
     tableName: 'User',
     columns: {
         id: { primary: true, type: 'uuid', generated: 'uuid' },
-        name: { type: 'varchar' },
+        username: { type: 'varchar' },
         email: { type: 'varchar' },
         password: { type: 'varchar' },
         isadmin: { type: 'boolean' },
