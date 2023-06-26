be5e545aff6ddf2b8d26630fd1caa6348241c174
TF8088 <tiagofarinha03@outlook.com>
2023-06-26 11:00:53 +0100

    WebSite Routes Changed to other file

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -8,9 +8,14 @@ websiteRoute.get("/", (req, res) => {
 websiteRoute.get("/city", async (req, res) => {
     const sensor = req.query.sensor
     res.render("city", { sensor: sensor });
-})
+});
 websiteRoute.get("/login", async (req, res) => {
     res.render("login");
-})
-
+});
+websiteRoute.get("/dashboard", async (req, res) => {
+    res.render("dashboard");
+});
+websiteRoute.get("/dashboard/sensor", async (req, res) => {
+    res.render("sensor");
+});
 module.exports = websiteRoute;
