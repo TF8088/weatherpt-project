0a75dff12ea5127eaec2b3579128e569ca462b72
TF8088 <tiagofarinha03@outlook.com>
2023-06-26 11:00:28 +0100

    Auth Code

--- src/shared/auth.js
+++ src/shared/auth.js
@@ -4,48 +4,35 @@ const LocalStrategy = require('passport-local').Strategy;
 const UserSchema = require('../database/entity/userEntity');
 const postUserTable = dataSource.getRepository(UserSchema);
 
-// function userAuthenticate2(username, password, done) {
-//   /* Alterar codigo Para o meu Projeto Fonte: Documenteção */
-//   // postUserTable.findOne({ where: { username: username } }, function (err, user) {
-//   //   console.log(user)
-//   // })
-//   console.log("Teste");
-//   // User.findOne({ username: username }, function (err, user) {
-//   //   if (err) { return done(err); }
-//   //   if (!user) { return done(null, false); }
-//   //   if (!user.verifyPassword(password)) { return done(null, false); }
-//   //   return done(null, user);
-//   // });
-// }
-var userAuthenticate = new LocalStrategy(async (username, password, done) => {
+const authenticateUser = async (email, password, done) => {
+  console.log("authenticateUser")
   try {
-    const user = await postUserTable.findOne({ where: { username: username } });
+    const user = await postUserTable.findOne({ where: { email: email } });
+    console.log(JSON.stringify(user, null, 2));
     if (!user) {
+      console.log("inside not user"); 
       return done(null, false);
     }
-    // Implemente a lógica de verificação da senha aqui
-    // Comparando a senha do usuário com a senha fornecida no callback
-
     if (password !== user.password) {
       return done(null, false, { message: 'Senha incorreta' });
     }
-
     return done(null, user);
   } catch (err) {
     console.log('Erro:', err);
     return done(err);
   }
-});
+};
 
+const serializeUser = async (user, done) => {
+  console.log(user);
+  return done(null, user);
+};
 
-const serializeUser = (function (user, done) {
-  done(null, user.id);
-});
-
-const deserializeUser = (function (id, done) {
+const deserializeUser = async (id, done) => {
+  console.log("deserializeUser"); 
   User.findById(id, function (err, user) {
-    done(err, user);
+    return done(err, user);
   });
-});
+}
 
-module.exports = { serializeUser, deserializeUser, userAuthenticate };
+module.exports = { serializeUser, deserializeUser, authenticateUser };
